648ddab6a23eb1b4fd7344fff124c936c0e1e470
Hans-Werner Hilse <hwhilse@gmail.com>
2015-05-25 21:12:54 +0200

    fixes

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)/linux
 cp ../linux.config ./.config
+[ -f linux ] && mv linux linux.O
 make -j4 ARCH=um &&
 make ARCH=um INSTALL_MOD_PATH=$(pwd)/../root-modules/ modules_install
