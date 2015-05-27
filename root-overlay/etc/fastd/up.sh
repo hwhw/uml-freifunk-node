1a0de6f76738452bd9a16825b7bbf3e094300ac0
Hans-Werner Hilse <hwhilse@gmail.com>
2015-05-27 12:28:02 +0200

    fixes

--- root-overlay/etc/fastd/up.sh
+++ root-overlay/etc/fastd/up.sh
@@ -5,7 +5,7 @@ if [ -n "$fastd_mac" ]; then
 fi
 ip link set dev $INTERFACE up
 
-if [ "$batman_version" == "14" ]; then
+if [ "$batman_version" = "14" ]; then
     echo bat0 > /sys/class/net/$INTERFACE/batman_adv14/mesh_iface
 else
     echo bat0 > /sys/class/net/$INTERFACE/batman_adv/mesh_iface
